b9402ca007efd23ccb633275ea571053a53899be
Arnaud-Lyard <arnaud.lyard@gmail.com>
2023-03-10 20:00:44 +0100

    🔥 clean app

--- app/src/pages/Dashboard.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="dashboard-title-section">
-    <h1 class="dashboard-title-section__title">
-      Admin <span class="dashboard-title-section__subtitle">Dashboard</span>
-    </h1>
-  </div>
-</template>
-<script setup lang="ts"></script>
